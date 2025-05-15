2a65badbf903b0e1d0decde7c9b4fc5b75429067
delperdang <steve.delperdang@gmail.com>
2025-05-15 21:08:44 +0000

    added logging and manual change detection to apologetics

--- src/app/apologetics/apologetics.component.ts
+++ src/app/apologetics/apologetics.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 interface ApologeticsTopic {
   code: string;
@@ -21,11 +22,19 @@ interface ApologeticsTopic {
 export class ApologeticsComponent implements OnInit {
   apologeticsTopics: ApologeticsTopic[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.fetchApologeticsTopics().subscribe(topics => {
+      console.log('Data received from API:', topics);
       this.apologeticsTopics = topics;
+      this.cdr.detectChanges();
+      console.log('apologeticsTopics after assignment:', this.apologeticsTopics);
+    }, error => {
+      console.error('Error fetching data:', error);
     });
   }
 
