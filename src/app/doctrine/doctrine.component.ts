9de39714cd28c62636ae9241f80dd88912ed85e6
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:29:10 +0000

    doctrine component

--- src/app/doctrine/doctrine.component.ts
+++ src/app/doctrine/doctrine.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface DoctrineTopic {
+  id: number;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-doctrine',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './doctrine.component.html',
   styleUrl: './doctrine.component.css'
 })
-export class DoctrineComponent {
+export class DoctrineComponent implements OnInit {
+  doctrineTopics: DoctrineTopic[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchDoctrineTopics().subscribe(topics => {
+      this.doctrineTopics = topics;
+    });
+  }
+
+  fetchDoctrineTopics(): Observable<DoctrineTopic[]> {
+    return this.http.get<DoctrineTopic[]>('https://delperdang.pythonanywhere.com/doctrine/');
+  }
 }
