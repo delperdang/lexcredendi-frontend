4972b7e918a660aca7d2c3c0f6fc3c9e2a49c69e
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:33:00 +0000

    litcal component

--- src/app/litcal/litcal.component.ts
+++ src/app/litcal/litcal.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface LiturgicalCalendarEntry {
+  id: number;
+  date: string;
+  name: string;
+  rank: string;
+  color: string;
+  season: string;
+}
 
 @Component({
   selector: 'app-litcal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './litcal.component.html',
   styleUrl: './litcal.component.css'
 })
-export class LitcalComponent {
+export class LitcalComponent implements OnInit {
+  liturgicalCalendar: LiturgicalCalendarEntry[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchLiturgicalCalendar().subscribe(calendar => {
+      this.liturgicalCalendar = calendar;
+    });
+  }
+
+  fetchLiturgicalCalendar(): Observable<LiturgicalCalendarEntry[]> {
+    return this.http.get<LiturgicalCalendarEntry[]>('https://delperdang.pythonanywhere.com/litcal/');
+  }
 }
