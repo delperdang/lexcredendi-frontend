0fbb667fe949055c3239c31af06ee2bc250b1a4f
delperdang <steve.delperdang@gmail.com>
2025-05-15 17:32:35 +0000

    Added home and did some additional initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule, MatToolbarModule, MatSidenavModule, MatListModule, MatIconModule,BrowserModule, BrowserAnimationsModule],
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ApologeticsComponent } from './apologetics/apologetics.component';
+import { ArtComponent } from './art/art.component';
+import { BibleComponent } from './bible/bible.component';
+import { CatechismComponent } from './catechism/catechism.component';
+import { DoctrineComponent } from './doctrine/doctrine.component';
+import { LitcalComponent } from './litcal/litcal.component';
+import { MeditationComponent } from './meditation/meditation.component';
+import { PrayerComponent } from './prayer/prayer.component';
+import { ReadingsComponent } from './readings/readings.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'apologetics', component: ApologeticsComponent },
+  { path: 'art', component: ArtComponent },
+  { path: 'bible', component: BibleComponent },
+  { path: 'catechism', component: CatechismComponent },
+  { path: 'doctrine', component: DoctrineComponent },
+  { path: 'litcal', component: LitcalComponent },
+  { path: 'meditation', component: MeditationComponent },
+  { path: 'prayer', component: PrayerComponent },
+  { path: 'readings', component: ReadingsComponent },
+  { path: '**', redirectTo: '' },
+];
