0ba6072e18b2206d48f8909c8c23e3c4ab63e859
delperdang <steve.delperdang@gmail.com>
2025-05-15 23:50:45 +0000

    removed deprecated httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } fr
 import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
-    importProvidersFrom(HttpClientModule)
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
