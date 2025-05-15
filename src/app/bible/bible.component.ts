45fc1eabe5063ac6f6cb2dd555c25427db0e8129
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:21:19 +0000

    bible component

--- src/app/bible/bible.component.ts
+++ src/app/bible/bible.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface BibleVerse {
+  id: number;
+  book: string;
+  chapter: number;
+  verse: number;
+  content: string;
+}
 
 @Component({
   selector: 'app-bible',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './bible.component.html',
   styleUrl: './bible.component.css'
 })
-export class BibleComponent {
+export class BibleComponent implements OnInit {
+  bibleVerses: BibleVerse[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchBibleVerses().subscribe(verses => {
+      this.bibleVerses = verses;
+    });
+  }
+
+  fetchBibleVerses(): Observable<BibleVerse[]> {
+    return this.http.get<BibleVerse[]>('https://delperdang.pythonanywhere.com/bible/');
+  }
 }
