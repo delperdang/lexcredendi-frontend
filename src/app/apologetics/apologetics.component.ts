992058bd1ef9f2cd269d0e51003b212ebda2408d
delperdang <steve.delperdang@gmail.com>
2025-05-15 17:41:39 +0000

    setup apologetics component

--- src/app/apologetics/apologetics.component.ts
+++ src/app/apologetics/apologetics.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface ApologeticsTopic {
+  id: number;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-apologetics',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatExpansionModule],
   templateUrl: './apologetics.component.html',
   styleUrl: './apologetics.component.css'
 })
-export class ApologeticsComponent {
+export class ApologeticsComponent implements OnInit {
+  apologeticsTopics: ApologeticsTopic[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchApologeticsTopics().subscribe(topics => {
+      this.apologeticsTopics = topics;
+    });
+  }
+
+  fetchApologeticsTopics(): Observable<ApologeticsTopic[]> {
+    return this.http.get<ApologeticsTopic[]>('https://delperdang.pythonanywhere.com/apologetics/');
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  newsItems = [
+    { title: 'Welcome to LexCredendi', content: 'This is the new home page for the LexCredendi web app.  We are currently under construction.' },
+    { title: 'Apologetics Section Coming Soon', content: 'Exciting updates are coming to the Apologetics section. Stay tuned!' },
+    { title: 'Explore the Bible', content: 'Dive deep into scripture with our Bible resources.' }
+  ];
 }
