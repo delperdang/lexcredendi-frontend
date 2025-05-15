5af9622a9ba3d86e5fd0bc7074e103b7bd302c40
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:24:46 +0000

    catechism component

--- src/app/catechism/catechism.component.ts
+++ src/app/catechism/catechism.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface CatechismTopic {
+  id: number;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-catechism',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './catechism.component.html',
   styleUrl: './catechism.component.css'
 })
-export class CatechismComponent {
+export class CatechismComponent implements OnInit {
+  catechismTopics: CatechismTopic[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchCatechismTopics().subscribe(topics => {
+      this.catechismTopics = topics;
+    });
+  }
+
+  fetchCatechismTopics(): Observable<CatechismTopic[]> {
+    return this.http.get<CatechismTopic[]>('https://delperdang.pythonanywhere.com/catechism/');
+  }
 }
