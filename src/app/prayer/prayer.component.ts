1a5372860d30622e79465e5c5ce770a8848e70cc
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:39:11 +0000

    prayer component

--- src/app/prayer/prayer.component.ts
+++ src/app/prayer/prayer.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface Prayer {
+  id: number;
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-prayer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './prayer.component.html',
   styleUrl: './prayer.component.css'
 })
-export class PrayerComponent {
+export class PrayerComponent implements OnInit {
+  prayers: Prayer[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchPrayers().subscribe(prayers => {
+      this.prayers = prayers;
+    });
+  }
+
+  fetchPrayers(): Observable<Prayer[]> {
+    return this.http.get<Prayer[]>('https://delperdang.pythonanywhere.com/prayer/');
+  }
 }
