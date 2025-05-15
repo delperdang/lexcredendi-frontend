1289f58eaa4cb6d750882ab80ed65ab1f1f5895c
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:40:41 +0000

    readings component

--- src/app/readings/readings.component.ts
+++ src/app/readings/readings.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatListModule } from '@angular/material/list';
+
+interface Reading {
+  id: number;
+  date: string;
+  first_reading: string;
+  psalm: string;
+  second_reading: string;
+  gospel: string;
+  feast_day: string;
+  color: string;
+  season: string;
+}
 
 @Component({
   selector: 'app-readings',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './readings.component.html',
   styleUrl: './readings.component.css'
 })
-export class ReadingsComponent {
+export class ReadingsComponent implements OnInit {
+  readings: Reading[] = [];
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.fetchReadings().subscribe(readings => {
+      this.readings = readings;
+    });
+  }
+
+  fetchReadings(): Observable<Reading[]> {
+    return this.http.get<Reading[]>('https://delperdang.pythonanywhere.com/readings/');
+  }
 }
