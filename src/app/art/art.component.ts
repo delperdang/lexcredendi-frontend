90af2be1c3b423ff60a3424d4d82977659714a0f
delperdang <steve.delperdang@gmail.com>
2025-05-15 19:15:57 +0000

    art component

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface ArtImage {
+  id: number;
+  title: string;
+  artist: string;
+  image: string;
+  description: string;
+  created_at: string;
+  updated_at: string;
+}
 
 @Component({
   selector: 'app-art',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatGridListModule, MatCardModule],
   templateUrl: './art.component.html',
   styleUrl: './art.component.css'
 })
-export class ArtComponent {
+export class ArtComponent implements OnInit {
+  artImages: ArtImage[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchArtImages().subscribe(images => {
+      this.artImages = images;
+    });
+  }
+
+  fetchArtImages(): Observable<ArtImage[]> {
+    return this.http.get<ArtImage[]>('https://delperdang.pythonanywhere.com/art/');
+  }
 }
